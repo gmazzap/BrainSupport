7f18f138b02bcae706745e59069b986cd2ad8520
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-05-15 18:55:13 +0200

    Introduced Facade

---
+++ src/Facade.php
@@ -0,0 +1,59 @@
+<?php namespace Brain;
+
+use \Brain\Container as Brain;
+
+/**
+ * Facade Class
+ *
+ * This class is a sort of *proxy* to ease the modules API calls.
+ * Concrete implementation are used to call methods defined in a Brain module
+ * using static methods, like:
+ *
+ *     Brain\FacadeName::fooMethod( $foo, $bar, $baz );
+ *
+ * Same methods can be also called using dynamic methods:
+ *
+ *     $api = new Brain\FacadeName();
+ *     $api->fooMethod( $foo, $bar, $baz );
+ *
+ * This is useful when the package is used inside OOP plugins, making use of dependency injection.
+ *
+ */
+abstract class Facade {
+
+    private static $container;
+
+    abstract static function getBindId();
+
+    public static function setContainer( Brain $container ) {
+        self::$container = $container;
+        return static::$container;
+    }
+
+    public static function getContainer() {
+        return self::$container;
+    }
+
+    public static function api() {
+        return self::getContainer()->get( static::getBindId() );
+    }
+
+    public static function __callStatic( $name, $arguments ) {
+        $name = __CLASS__;
+        $id = static::getBindId();
+        $container = self::getContainer();
+        if ( ! $container instanceof Brain || ! static::api() instanceof Striatum\API ) {
+            return new \WP_Error( "{$id}-api-not-ready", "{$name} API object is not ready." );
+        }
+        if ( method_exists( static::api(), $name ) ) {
+            return call_user_func_array( [ static::api(), $name ], $arguments );
+        } else {
+            return new \WP_Error( "{$id}-api-invalid-call", "Invalid {$name} API call." );
+        }
+    }
+
+    public function __call( $name, $arguments ) {
+        return static::__callStatic( $name, $arguments );
+    }
+
+}
