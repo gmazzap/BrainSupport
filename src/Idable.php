1c44d962d7807fb946c76b332e2fa863dbe0ab27
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-12-02 08:51:21 +0100

    Remove default for Idable::setId()

--- src/Idable.php
+++ src/Idable.php
@@ -1,16 +1,19 @@
 <?php namespace Brain;
 
-trait Idable {
+trait Idable
+{
 
-    public function setId( $id = NULL ) {
-        if ( ! is_string( $id ) || empty( $id ) ) {
+    public function setId($id)
+    {
+        if ( ! is_string($id) || empty($id)) {
             throw new \InvalidArgumentException;
         }
         $this->id = $id;
         return $this;
     }
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
 
